1b16c5d2afb6586d20a5676f9d55aca47bad9550
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-04-16 10:35:57 +0200

    stores updater 01 - komentarze

--- stores_updater_01.py
+++ stores_updater_01.py
@@ -7,19 +7,27 @@ import producer_02
 class api_logic:
 
     def __init__(self):
+        # Poprzedni kontener dla przechowywania ratingow
         self.raw_ratings_data = []
         self.user_ratings_with_genres, self.genres_column_names = producer_02.get_user_ratings_with_movie_genres()
         self.avg_genre_ratings_for_user = {}
         self.user_profile = {}
+        # Wybrana przeze mnie nazwa kolejki dla listy wszystkich ratingow
         self.queue_name = "ratings_list"
+        # Redisowa kolejka przechowujaca ratingi
         self.redis_ratings_client = redis.StrictRedis(host='localhost', charset='utf-8', port=6381, db=2)
         self.redis_profiles_client = redis.StrictRedis(host='localhost', charset='utf-8', port=6381, db=1)
         self.ratings_list_name = "ratings_list"
 
 
-
+    # Metoda obslugujaca dodawanie new_rating do Redisowej listy
     def add_rating(self, new_rating):
+        # patrz redis_ratings_client
+        # Do redisowej kolejki przechowujacej ratingi dodajemy new_rating
+        # Za pomoca metody rpush(<nazwa_kolejki>, <dodawany rating>)
         self.redis_ratings_client.rpush(self.queue_name, json.dumps(new_rating))
+
+        # Poprzedni sposob, w ktorym zamiast dodawac do redisowej kolejki dodawalismy ratingi do prostej tablicy
         #self.raw_ratings_data.append(new_rating)
 
     def get_ratings(self):
