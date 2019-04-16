bbfbc6d77bb470f1c8b646b87bcf751d7ba3ba4f
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-04-16 11:07:04 +0200

    testing

--- requests_client_01.py
+++ requests_client_01.py
@@ -5,6 +5,38 @@ import time
 import json
 import requests
 
-sample_row = producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[0]
-r = requests.post(url='http://127.0.0.1:9875/rating', json=json.dumps(sample_row))
-print(r)
+sample_rows = []
+sample_rows.append(producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[0])
+sample_rows.append(producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[1])
+sample_rows.append(producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[2])
+sample_rows.append(producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[3])
+sample_rows.append(producer_02.get_dataframe_as_dict(producer_02.merge_user_ratings_with_movie_genres())[4])
+
+r = requests.post(url='http://127.0.0.1:9875/rating', json=json.dumps(sample_rows[0]))
+print(r.text)
+
+r2 = requests.post(url='http://127.0.0.1:9875/rating', json=json.dumps(sample_rows[1]))
+print(r2.text)
+
+r3 = requests.post(url='http://127.0.0.1:9875/rating', json=json.dumps(sample_rows[2]))
+print(r3.text)
+
+r4 = requests.post(url='http://127.0.0.1:9875/rating', json=json.dumps(sample_rows[3]))
+print(r4.text)
+
+r5 = requests.get(url='http://127.0.0.1:9875/ratings')
+print(r5.text)
+
+r6 = requests.delete(url='http://127.0.0.1:9875/ratings')
+print(r6.text)
+
+r7 = requests.get(url='http://127.0.0.1:9875/ratings')
+print(r7.text)
+
+r8 = requests.get(url='http://127.0.0.1:9875/avg-genre-ratings/all-users')
+print(r8.text)
+
+r9 = requests.get(url='http://127.0.0.1:9875/avg-genre-ratings/75')
+print(r9.text)
+
+
